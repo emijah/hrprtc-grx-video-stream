6fcf3e678fbccd82388e0e59381c2dddb438d379
kawasumi <kawasumi@generalrobotix.com>
2011-09-23 23:54:35 +0900

    capture 5 times temporalily

--- sample.py
+++ sample.py
@@ -8,9 +8,4 @@ vs = rtm.findRTC('VideoStream0')
 vs.start()
 vs_svc = Img.CameraCaptureServiceHelper.narrow(vs.service('service0'))
 
-## capture coninuous
 vs_svc.take_one_frame()
-#import time 
-#time.sleep(10)
-## stop capture
-#vs_svc.capture(0)
