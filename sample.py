4ed2773b597167c94b990b2dee81a0f7a05e643d
kawasumi <kawasumi@generalrobotix.com>
2011-09-29 15:28:00 +0900

    add gui.py

--- sample.py
+++ sample.py
@@ -4,8 +4,21 @@ sys.path.append('Img.jar')
 import rtm
 import Img.CameraCaptureServiceHelper
 
-vs = rtm.findRTC('VideoStream0')
-vs.start()
-vs_svc = Img.CameraCaptureServiceHelper.narrow(vs.service('service0'))
+def init():
+  global vs_svc
+  vs = rtm.findRTC('VideoStream0')
+  vs.start()
+  vs_svc = Img.CameraCaptureServiceHelper.narrow(vs.service('service0'))
 
-vs_svc.take_one_frame()
+  # if you setup MultiDisp RTC (OpenVGR)
+  md = rtm.findRTC('MultiDisp0')
+  if md != None:
+    rtm.connectPorts(vs.port('MultiCameraImages'), md.port('images'))
+    md.start()
+
+def take_one_frame():
+  vs_svc.take_one_frame()
+
+if __name__ == '__main__' or __name__ == 'main':
+  init()
+  take_one_frame()
